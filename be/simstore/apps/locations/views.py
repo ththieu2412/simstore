ae981d5c2d251f469d71610afdb5ab7512bff676
TrungHieu <ththieu2412@gmail.com>
2025-03-07 12:12:02 +0700

    Updated Location

--- be/simstore/apps/locations/views.py
+++ be/simstore/apps/locations/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import Province, District, Ward
 from .serializers import ProvinceSerializer, DistrictSerializer, WardSerializer
+from rest_framework.views import APIView
 
 class CustomViewSet(viewsets.ModelViewSet):
     """Custom ViewSet để chuẩn hóa format JSON trả về"""
@@ -50,3 +51,37 @@ class DistrictViewSet(CustomViewSet):
 class WardViewSet(CustomViewSet):
     queryset = Ward.objects.all()
     serializer_class = WardSerializer
+
+class DistrictViewSet(CustomViewSet):
+    """
+    API lấy danh sách quận theo province_id.
+    """
+    queryset = District.objects.all()
+    serializer_class = DistrictSerializer
+
+    def list(self, request, *args, **kwargs):
+        province_id = request.GET.get('province_id')
+
+        if not province_id:
+            return self.format_response(status.HTTP_400_BAD_REQUEST, None, "error", "province_id is required")
+
+        districts = District.objects.filter(province_id=province_id)
+        serializer = self.get_serializer(districts, many=True)
+        return self.format_response(status.HTTP_200_OK, serializer.data)
+    
+class WardViewSet(CustomViewSet):
+    """
+    API lấy danh sách phường dựa vào district_id.
+    """
+    queryset = Ward.objects.all()
+    serializer_class = WardSerializer
+
+    def list(self, request, *args, **kwargs):
+        district_id = request.GET.get('district_id')
+
+        if not district_id:
+            return self.format_response(status.HTTP_400_BAD_REQUEST, None, "error", "district_id is required")
+
+        wards = Ward.objects.filter(district_id=district_id)
+        serializer = self.get_serializer(wards, many=True)
+        return self.format_response(status.HTTP_200_OK, serializer.data)
